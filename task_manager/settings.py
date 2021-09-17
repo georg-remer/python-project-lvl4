f6c2cb609d52bdbd2b9223ecba0317c7ea668a7c
George Rymarenko <george.rymarenko@icloud.com>
2021-09-17 12:36:49 +0200

    chore: use os.getenv to load variables

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,25 +12,27 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables
-config = dotenv_values(BASE_DIR / '.env')
+# config = dotenv_values(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('SECRET_KEY', 'django-insecure-cm80m^a7haem5m*np4)k5648u*#y_&n4l^66-#%)99!_i=wnm2')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(config.get('DEBUG', '0')))
+DEBUG = bool(int(os.getenv('DEBUG', '0')))
 
-ALLOWED_HOSTS = config.get('ALLOWED_HOSTS', '').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
 
 # Application definition
