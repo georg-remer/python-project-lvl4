6c9d6e4ecdae318983c4616673a25e9c75d20589
George Rymarenko <george.rymarenko@icloud.com>
2021-09-27 19:51:52 +0200

    fix: try success_url

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -19,8 +19,9 @@ class UserCreate(SuccessMessageMixin, CreateView):
     template_name = 'users/create.html'
     model = get_user_model()
     form_class = UserForm
+    # success_url = reverse_lazy('login')
+    success_url = '/login'
     success_message = _('User was successfully created')
-    success_url = reverse_lazy('login')
 
 
 class UserUpdate(PermissionDeniedMixin, SelfCheckMixin, SuccessMessageMixin, UpdateView):
