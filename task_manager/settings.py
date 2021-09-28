7598403ffcbfb2130c26aacfa38798be143115af
George Rymarenko <george.rymarenko@icloud.com>
2021-09-28 17:04:34 +0200

    chore: update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -128,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'users.User'
 
-# LOGIN_URL = reverse_lazy('login')
+LOGIN_URL = reverse_lazy('login')
 LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGOUT_REDIRECT_URL = reverse_lazy('index')
 
